16f0d095342eb08df60d97cb22feae128d288b72
root <root@ip-172-31-5-78.ap-northeast-2.compute.internal>
2021-08-12 11:33:38 +0900

    django 앱 생성

---
+++ carbon_diet/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for carbon_diet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os, sys
+
+from django.core.wsgi import get_wsgi_application
+
+path = os.path.abspath(__file__+'/../..')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carbon_diet.settings')
+
+application = get_wsgi_application()
